8f9ed785e0b98e31771c5b07715b3f3ae84f4f2d
Nikita <keddofilatov@gmail.com>
2017-05-16 21:35:06 +0600

    [+] header

---
+++ src/Components/@ComponentsExport.js
@@ -0,0 +1,3 @@
+import * as Components from './@ComponentsImport';
+
+export { Components }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.css';
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header__wrapper header__wrapper--left"></div>
+      <div className="header__wrapper header__wrapper--right"></div>
+    </div>
+  )
+}
